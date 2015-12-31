1e1530e0acef5b8ec2a6e4114b42a9526a19c15d
lanrion <huaitao-deng@foxmail.com>
2015-12-31 11:20:53 +0800

    removed old test

--- test/test_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'active_support/core_ext/hash/conversions'
-require 'minitest/autorun'
-require 'wx_pay'
-require 'fakeweb'
-
-WxPay.appid = 'wxd930ea5d5a258f4f'
-WxPay.key = '8934e7d15453e97507ef794cf7b0519d'
-WxPay.mch_id = '1900000109'
-WxPay.apiclient_cert_path = '/path/to/your/cert/file.p12'
